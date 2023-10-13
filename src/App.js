a91df064488e8c19dab34a3ef90df9dada7574c8
Varun Vats <47187283+Varun-Vats-Jarvis@users.noreply.github.com>
2023-10-13 19:03:13 +0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = ()=> {
             
           />
         <Routes>
-        <Route path='/' element={<Recipe  key='indian' setProgress={setProgress} apiKey={apiKey} cuisine='indian'number = {20} />}></Route>
+        <Route path='/React-App' element={<Recipe  key='indian' setProgress={setProgress} apiKey={apiKey} cuisine='indian'number = {20} />}></Route>
         <Route path='/mexican' element={<Recipe  key='mexican' setProgress={setProgress} apiKey={apiKey} cuisine='mexican' number = {20}/>}></Route>
         <Route path='/chinese' element={<Recipe  key='chinese' setProgress={setProgress} apiKey={apiKey} cuisine='chinese' number = {20} />}></Route>
         <Route path='/italian' element={<Recipe  key='italian' setProgress={setProgress} apiKey={apiKey} cuisine='italian'number = {20} />}></Route>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,9 +12,6 @@ const NavBar = () => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                          
-
-
                             <li className="nav-item">
                                 <Link className="nav-link" to="/mexican">Mexican</Link>
                             </li>
